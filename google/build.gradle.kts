81992dd66acd7eec2f52dfc4e036c4e2974f5f85
YCX-C <ycx974577817@gmail.com>
2025-03-20 14:29:43 +0800

    新增 谷歌账号 操作类

---
+++ google/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("someone.code.library")
+}
+
+android {
+    namespace = "com.usecase.google"
+}
+
+dependencies {
+
+}
---
+++ google/src/main/java/com/usecase/google/GoogleAccountUseCase.kt
@@ -0,0 +1,70 @@
+package com.usecase.google
+
+import android.accounts.Account
+import android.accounts.AccountManager
+import android.accounts.AccountManagerFuture
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+public class GoogleAccountUseCase(private val context: Context) {
+
+    private companion object {
+        private const val GOOGLE_ACCOUNT_TYPE = "com.google"
+        private const val GOOGLE_AUTH_TOKEN_TYPE = "androidmarket"
+    }
+
+    private val accountManager by lazy { AccountManager.get(context) }
+
+    private fun futureStartActivity(future: AccountManagerFuture<Bundle>) {
+
+        @Suppress("DEPRECATION")
+        val intent = future.result.getParcelable<Intent>("intent") ?: return
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        context.startActivity(intent)
+    }
+
+    public fun getAccountList(): List<Account> {
+        return accountManager.getAccountsByType(GOOGLE_ACCOUNT_TYPE).toList()
+    }
+
+    private suspend fun removeAccount(account: Account) = suspendCoroutine { continuation ->
+        Log.d("GoogleAccountUseCase", "removeAccount: ${account.name}")
+
+        accountManager.removeAccount(account, null, { futureRemoveAccount(it, continuation) }, null)
+    }
+
+    private fun futureRemoveAccount(
+        future: AccountManagerFuture<Bundle>,
+        continuation: Continuation<Unit>
+    ) {
+        futureStartActivity(future)
+        continuation.resume(Unit)
+    }
+
+    public suspend fun removeAllAccount() {
+        for (account in getAccountList()) {
+            removeAccount(account)
+        }
+    }
+
+    public fun launchGoogleLogin() {
+        accountManager.addAccount(
+            GOOGLE_ACCOUNT_TYPE,
+            GOOGLE_AUTH_TOKEN_TYPE,
+            null,
+            null,
+            null,
+            ::futureStartActivity,
+            null
+        )
+    }
+
+}
